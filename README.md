52836bc05ec4aacc2979bd4dc541228318ed0331
Chasoe <33845931+Chasoe@users.noreply.github.com>
2018-04-09 10:46:38 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Version-styring-test
+# GrowMyBank_Game_Project
+small school project
+
+Assignment is to primary test the basic concept of threading using GDI+ and windows Forms.
+this is done by creating a small game.(windows forms is not an ideal Framework for this but whatever)
+in the game you control a bank with some workers assigned, thier job is to go mine for gold and coal.
---
+++ Rts_project_base/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rts_project_base")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rts_project_base")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("73eb01c4-3923-4709-bd4b-2e584d7813c8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
